2b05085e7e60966e5496d7bdaa640ba619bafb93
Jmunozi <43503590+Jmunozi@users.noreply.github.com>
2024-02-05 18:45:25 -0300

    Update classname

--- src/SocialButton.js
+++ src/SocialButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SocialButton = ({ image, onClick }) => (
-  <button onClick={onClick} className="btn btn-light m-2 social-button">
+  <button onClick={onClick} className="btn btn-light m-2 social-button justify-content-center align-items-center minh-100">
     <img src={image} alt="Social Icon" className="social-icon" />
   </button>
 );
